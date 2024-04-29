748685a580b8b093cb991a97439ad3d420192411
Jair0818 <naruhina0818@gamil.com>
2024-04-29 16:52:49 -0600

    added .gitignore

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+const morgan=require('morgan');
 const express=require('express');
 const app=express();
-/*
-Verbos HTTP
+//Routes
+const pokemon=require('./routes/pokemon');
+const user=require('./routes/user');
+//Middleware
+const auth=require('./middleware/auth');
+const notFoud=require('./middleware/notFoud');
+const index=require('./middleware/index');
+/*Verbos HTTP
 GET es lo que ejecuta los navegadores 
 POST es cuando queremos guardar o publicar algo 
 PATCH es la utilizacion de un dato por especifico 
 PUT modificar todos los elementos
-DELETE elimitar un recurso
-*/
-app.get("/",(req,res,next)=>{
-    res.status(200);
-    res.send("Hola como estas");
-});
-app.listen(3000,()=>{
+DELETE elimitar un recurso*/
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+/*app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));*/
+app.get("/",index);
+app.use("/user",user);
+app.use(auth);
+app.use("/pokemon",pokemon);
+app.use(notFoud);
+app.listen(process.env. PORT||3000,()=>{
     console.log("Server is runnig..")
 });
