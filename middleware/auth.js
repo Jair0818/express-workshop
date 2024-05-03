10918455289a649f3fa78d21bb59c732894a4d14
Jair0818 <naruhina0818@gamil.com>
2024-05-03 13:37:25 -0600

    inicial commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt=require('jsonwebtoken');
 module.exports=(req,res,next)=>{
     try{
+        console.log(req.headers);
         const token=req.headers.authorization.split(" ")[1];
         const decoded=jwt.verify(token,"debugkey");
         req.user=decoded;
